7645123ca76d150f8e707db25ab09f9b71579919
anusmariya <tmariya93@gmail.com>
2024-04-23 11:50:29 +0530

    code added by mariya

--- src/com/mariya/GitHUb.java
+++ src/com/mariya/GitHUb.java
@@ -4,7 +4,10 @@ public class GitHUb {
 
 	private void sysout() {
 	System.out.println("maiya");
-
 	}
-	
+	public static void main(String[] args) {
+		GitHUb b = new GitHUb();
+		b.sysout();
+		
+	}
 }
